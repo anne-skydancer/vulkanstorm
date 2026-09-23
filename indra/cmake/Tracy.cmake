26dbdc2d8fe3b73b03c6ebfca589ba931d9dc58f
Anne Skydancer <anne.skydancer@outlook.com>
2026-09-23 04:17:50 +0100

    Keep Tracy memory and GPU instrumentation opt-in on development

--- indra/cmake/Tracy.cmake
+++ indra/cmake/Tracy.cmake
@@ -16,12 +16,17 @@ if (USE_TRACY)
   option(USE_TRACY_ON_DEMAND "Use on-demand Tracy profiling." ON)
   option(USE_TRACY_LOCAL_ONLY "Disallow remote Tracy profiling." OFF)
   option(USE_TRACY_GPU "Use Tracy GPU profiling" OFF)
+  option(USE_TRACY_MEMORY "Record individual allocation/free events in Tracy" OFF)
 
   use_prebuilt_binary(tracy)
 
   target_include_directories( ll::tracy SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include/tracy)
 
   target_compile_definitions(ll::tracy INTERFACE -DTRACY_ENABLE=1 -DTRACY_ONLY_IPV4=1)
+
+  if (USE_TRACY_MEMORY)
+    target_compile_definitions(ll::tracy INTERFACE -DLL_PROFILER_ENABLE_TRACY_MEMORY=1)
+  endif ()
 
   if (USE_TRACY_ON_DEMAND)
     target_compile_definitions(ll::tracy INTERFACE -DTRACY_ON_DEMAND=1)
@@ -38,4 +43,3 @@ if (USE_TRACY)
   # See: indra/llcommon/llprofiler.h
   add_compile_definitions(LL_PROFILER_CONFIGURATION=3)
 endif (USE_TRACY)
-
